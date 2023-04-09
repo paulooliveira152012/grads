3573932a014480955da0303adeb8987b484c49c7
paulooliveira152012 <paulooliveira152012@gmail.com>
2023-04-09 00:16:19 -0400

    adding js for slide

---
+++ js.js
@@ -0,0 +1,22 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide() {
+  // hide all slides
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+
+  // show current slide
+  slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide > slides.length - 1) {
+    currentSlide = 0;
+  }
+  showSlide();
+}
+
+setInterval(nextSlide, 5000); // change slide every 5 seconds
